6b89bdc233f6fc64fca65d8a4e393841fc5744b0
Angel Romero <mrangelrcd@gmail.com>
2023-01-04 23:30:57 +0100

    Added functionality to switch binary/decimal

--- src/components/MainApp.jsx
+++ src/components/MainApp.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
-import { useState } from 'react';
-import { binaryToDecimal } from '../modules/bin2dec';
+import Switch from '@mui/material/Switch';
+import { useState, useRef } from 'react';
+import { binaryToDecimal, decimalToBinary } from '../modules/bin2dec';
 
 function MainApp() {
-	const [inputValue, setInputValue] = useState();
+	const [inputValue, setInputValue] = useState('');
+	const [switchState, setSwitchState] = useState(true);
+	const convertedNumber = useRef('');
+
+	const handleChange = (event) => {
+		if (convertedNumber.current.innerHTML !== 'Introduce un valor'){
+			setInputValue(convertedNumber.current.innerHTML);
+		}
+		setSwitchState(event.target.checked);
+		console.log(switchState);
+	};
+
+	const convertedNum = (inputValue) => {
+		// console.log(convertedNumber.current.innerHTML);
+		return switchState ? binaryToDecimal(inputValue) : decimalToBinary(inputValue);
+	}; 
 
 	return (
 		<div className='w-screen h-screen bg-slate-300 flex flex-col justify-center items-center'>
-			<h1>BIN<span className='text-2xl'>2</span>DEC</h1>
+			<h1 className='text-3xl'>BIN<span className='text-5xl text-blue-600'>2</span>DEC</h1>
+			<div className='flex items-center'>
+				<p>Convertir a Binario</p>
+				<Switch
+					checked={switchState}
+					onChange={handleChange}
+					inputProps={{ 'aria-label': 'controlled' }}
+				/>
+				<p>Convertir a Decimal</p>
+			</div>
 			<input value={inputValue} onChange={(event) => setInputValue(event.target.value)} className='text-center' type="text" />
-			<p>{binaryToDecimal(inputValue)}</p>
+			
+			<p ref={convertedNumber}>{convertedNum(inputValue)}</p>
 		</div>
 	);
 }
--- src/modules/bin2dec.js
+++ src/modules/bin2dec.js
@@ -1,5 +1,7 @@
 export function binaryToDecimal(binary){
+	if (binary === '') return 'Introduce un valor';
 	binary = binary.toString();
+	if(!/^[01]+$/.test(binary)) return 'Valor no válido';
 	let decimal = 0;
 	for (let i = 0; i < binary.length; i++) {
 		decimal += binary[i]* (2**(binary.length-1-i));
@@ -7,4 +9,8 @@ export function binaryToDecimal(binary){
 	return decimal;
 }
 
-console.log(binaryToDecimal(1111101011));
+export function decimalToBinary(decimal){
+	if (decimal === '') return 'Introduce un valor';
+	if(!/^[0-9]+$/.test(decimal)) return 'Valor no válido';
+	return (decimal >>> 0).toString(2);
+}
